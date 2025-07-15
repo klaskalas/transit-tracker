0cfd80de04c1b57719b8851081ddbe2e7f10c81d
Klas <klas.nederman@gmail.com>
2025-07-15 23:45:29 +0200

    include agency and updated gtfsRouteType enum

--- TransitTrackerWebApi/Repositories/RoutesRepository.cs
+++ TransitTrackerWebApi/Repositories/RoutesRepository.cs
@@ -5,36 +5,32 @@ using NetTopologySuite.IO;
 
 namespace TransitTrackerWebApi.Repositories;
 
-public class RoutesRepository
+public class RoutesRepository(AppDbContext context)
 {
-    private readonly AppDbContext _context;
-
-    public RoutesRepository(AppDbContext context)
-    {
-        _context = context;
-    }
-
     public async Task<List<Route>> GetAllRoutesAsync()
     {
-        return await _context.Routes.Take(25).ToListAsync();
+        return await context.Routes
+            .Include(a => a.Agency)
+            .Take(25)
+            .ToListAsync();
     }
 
     public async Task<Route?> GetRouteByIdAsync(int id)
     {
-        return await _context.Routes.FindAsync(id);
+        return await context.Routes.FindAsync(id);
     }
 
     public async Task<Route> AddRouteAsync(Route route)
     {
-        _context.Routes.Add(route);
-        await _context.SaveChangesAsync();
+        context.Routes.Add(route);
+        await context.SaveChangesAsync();
         return route;
     }
 
     public async Task<bool> UpdateRouteAsync(Route route)
     {
-        _context.Routes.Update(route);
-        var affected = await _context.SaveChangesAsync();
+        context.Routes.Update(route);
+        var affected = await context.SaveChangesAsync();
         return affected > 0;
     }
 
@@ -43,16 +39,16 @@ public class RoutesRepository
         var route = await GetRouteByIdAsync(id);
         if (route == null) return false;
         
-        _context.Routes.Remove(route);
-        var affected = await _context.SaveChangesAsync();
+        context.Routes.Remove(route);
+        var affected = await context.SaveChangesAsync();
         return affected > 0;
     }
     
     public async Task<string> GetRouteGeoJsonFeatureCollectionAsync(int routeId)
     {
         var geometries = await (
-            from rs in _context.RouteShapes
-            join sl in _context.ShapeLines on rs.GtfsShapeId equals sl.GtfsShapeId
+            from rs in context.RouteShapes
+            join sl in context.ShapeLines on rs.GtfsShapeId equals sl.GtfsShapeId
             where rs.RouteId == routeId
             select sl.Geom
         ).ToListAsync();
